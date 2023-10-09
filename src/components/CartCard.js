04d8fdb0725cee87d8fe7d92a410f7386d9b400f
Aditya Navnit Patil <adityapatil687@gmail.com>
2023-10-09 23:49:49 +0530

    added toast notification to cart

--- src/components/CartCard.js
+++ src/components/CartCard.js
@@ -1,7 +1,7 @@
 import { useState, useEffect, useContext } from "react"
 import { CartContext } from "../context/CartContextProvider"
 
-const CartCard = ({data}) => {
+const CartCard = ({data, showToastMessage}) => {
 
     const {cartData, setCartData} = useContext(CartContext)
 
@@ -96,7 +96,7 @@ const CartCard = ({data}) => {
                             </div>
                         </div>
 
-                        <button className="col-1 me-3 btn btn-sm btn-success" onClick={()=>{deleteHandler(data.id)}}>
+                        <button className="col-1 me-3 btn btn-sm btn-success" onClick={()=>{showToastMessage();deleteHandler(data.id)}}>
                             {/* <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
                                 <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
                             </svg> */}
--- src/pages/CartScreen.js
+++ src/pages/CartScreen.js
@@ -6,13 +6,15 @@ import CartCard from "../components/CartCard";
 import Checkout from "../components/Checkout";
 import ReactDOM from "react-dom";
 import { CartContext } from "../context/CartContextProvider";
-
+import {Slide, ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const CartScreen = () => {
 
     const {cartData, setCartData} = useContext(CartContext)
 
     const [total, setTotal] = useState(0)
-    
+    const [showToast, setShowToast] = useState(false);
+
     useEffect(()=>{
             let element = document.getElementById('root')
             element.classList.remove("my-auto")
@@ -22,6 +24,7 @@ const CartScreen = () => {
             pageTitle.innerHTML='Cart'
     },[])
 
+
     useEffect(() => {
         // Calculate the total based on the updated cartData
         const newTotal = cartData.reduce(
@@ -29,15 +32,29 @@ const CartScreen = () => {
           0
         );
         setTotal(newTotal);
+        
       }, [cartData]);
 
     const deleteHandler = (id) =>
     {
+        
         const updatedCartData = cartData.filter((obj) => obj.id !== id);
         setCartData(updatedCartData);
     }
     
-   
+    
+    const showToastMessage = () => {
+        toast.success('Removed from cart', {
+            position: toast.POSITION.BOTTOM_RIGHT,
+            className: 'border rounded bg-light-subtle text-light-emphasis mb-5',
+            autoClose: 1000,
+            pauseOnHover: false,
+            transition: Slide,
+            progressStyle: {backgroundColor: "#198754"},
+            icon: false,
+            bodyClassName: 'bg-light-subtle',
+        });
+    };
     return (
         <>
         <div className="cart-screen" data-aos="fade-up" data-aos-duration="1000">
@@ -45,7 +62,7 @@ const CartScreen = () => {
                 {
                 (cartData !== null && cartData.length> 0) ?
                 cartData.map((obj) => {
-                    return <CartCard key={obj.id} data={obj}  deleteHandler={deleteHandler} />;
+                    return <CartCard key={obj.id} data={obj}  deleteHandler={deleteHandler} showToastMessage={showToastMessage} />;
                 })
                 :
                 (
@@ -54,6 +71,7 @@ const CartScreen = () => {
             </div>
             
         </div>
+            <ToastContainer />
             {ReactDOM.createPortal(<Checkout total={total}/>, document.getElementById("footer"))}
         </>
       );
