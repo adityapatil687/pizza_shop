35e34772821662970e8aec75696dc8a85097fc4e
Aditya Navnit Patil <adityapatil687@gmail.com>
2023-10-09 23:50:42 +0530

    index fix

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import CartContextProvider from './context/CartContextProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- 
+  <React.StrictMode>
     <CartContextProvider>
       <App />
     </CartContextProvider>
- 
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
